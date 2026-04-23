9cfb5d82ed2f666b1f2076b2faad705a4c1f1eb1
Isra Zhaku <iz32812@seeu.edu.mk>
2026-04-23 17:58:17 +0200

    Fetched and displayed posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error)
+          setError(error.message)
+          setLoading(false);
+        })
+  }, [])
+
+
+  if (loading) return <div>Loading...</div>
+
+  if (error) return <div>Error: {error}</div>;
+
 
   return (
-      <dev></dev>
-  )
-}
+      <div>
+        <h1>Posts</h1>
 
+        {posts.map((post) => {
+          return (
+              <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+          );
+        })}
+      </div>
+  );
+}
 export default App
