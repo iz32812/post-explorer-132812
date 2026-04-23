f7e536f41015b13812af266d3b0890aecf13d263
Isra Zhaku <iz32812@seeu.edu.mk>
2026-04-23 18:31:13 +0200

    Adedd PostCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { PostCard } from "./components/PostCard";
+import  PostCard  from "./components/PostCard";
 import "./App.css";
 
 function App() {
